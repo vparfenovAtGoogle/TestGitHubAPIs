3a8f0d691a7810ef5566adc07732d52b2acd5336
Vladimir Parfenov <vparfenov@ptc.com>
2019-08-07 00:14:28 -0400

    Added JWT token handling

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+const jwt = require('jsonwebtoken')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let headers = req.headers
+  const x_ms_client_principal = header ['x-ms-client-principal']
   headers = Object.keys (headers).map (key => {return {key, value: headers [key]}})
+  const claims = []
+  if (x_ms_client_principal) {
+    try {
+      const decoded = jwt.decode(x_ms_client_principal, {complete: true})
+      if (decoded && decoded.header) {
+        decoded.header.claims.map (claim => {claims.push ({typ: claim.typ, val: claim.val}) })
+      }
+    }
+    catch (err) {
+      // ignore
+    }
+  }
   res.render('index',
     {
       title: 'XPEH',
-      headers: headers,
+      headers,
+      claims,
       people: [
         {lastName: 'Parfenova', firstName: 'Olga', state: 'warning'},
         {lastName: 'Parfenova', firstName: 'Maria', state: 'info'},
