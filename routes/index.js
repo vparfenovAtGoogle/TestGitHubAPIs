1f9e2686fde22dc02ffaee7f5a07e4b8284d5437
Vladimir Parfenov <vparfenov@ptc.com>
2019-08-16 19:58:06 -0400

    Fixed People List

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,19 @@
 const express = require('express')
 const router = express.Router()
 
+const people = [
+  {lastName: 'Goubanov', firstName: 'Rodion', state: 'warning'},
+  {lastName: 'Apraj', firstName: 'Shashikant', state: 'info'},
+  {lastName: 'Dementiev', firstName: 'Daniel', state: 'danger'},
+  {lastName: 'Parfenov', firstName: 'Vladimir', state: 'success'}
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index',
     {
       title: 'Test Node.JS with AAD Authentication',
-      people: [
-        {lastName: 'Parfenova', firstName: 'Olga', state: 'warning'},
-        {lastName: 'Parfenova', firstName: 'Maria', state: 'info'},
-        {lastName: 'Parfenov', firstName: 'Vlad', state: 'danger'},
-        {lastName: 'Parfenov', firstName: 'Vladimir', state: 'success'}
-      ]
+      people
     })
 })
 
