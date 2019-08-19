8ef82e5b27f60b35b5eeed56f8a68c662dcbc657
Vladimir Parfenov <vparfenov@ptc.com>
2019-08-18 22:25:25 -0400

    Added post login/logout re-directions

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const router = express.Router()
 router.get('/login', (req, res) => {  
-  res.render('login')
+  res.render('login', { layout: 'loginlayout', title: "Login with one of the following Identity providers" })
 })
 router.get('/debug', (req, res) => {  
   let headers = req.headers
